b9706e3646e7bf4a05d7b181431658376f0a9845
abhishekkkk-15 <mrabhi748@gmail.com>
2025-05-10 21:50:10 +0530

    CORS implementation

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import { initRedis } from "./services/redis.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true, // Allow credentials (cookies)
   })
 );
